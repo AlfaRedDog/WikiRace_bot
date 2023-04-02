bc8d05634a2d3865b2450bc56a67f7dd9fad4b65
Igor <fdiii@mail.ru>
2023-04-02 11:07:04 +0300

    Refactor

--- src/main/kotlin/com/itmo/microservices/demo/kafka/MessageConsumer.kt
+++ src/main/kotlin/com/itmo/microservices/demo/kafka/MessageConsumer.kt
@@ -5,39 +5,36 @@ import com.itmo.microservices.demo.kafka.config.KafkaConfig
 import com.itmo.microservices.demo.kafka.models.ResponseStatusEnum
 import org.springframework.boot.configurationprocessor.json.JSONObject
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
 
 
 @Service
 class MessageConsumer(private val tokenManager: JwtTokenManager, private val messageProducer: MessageProducer) {
     @KafkaListener(topics = [KafkaConfig.Wiki_topic], groupId = KafkaConfig.Group_id)
-    fun consumeFromSubscribe(token: String): Unit {
+    fun consumeFromSubscribe(token: String) {
+        var responseMessage = JSONObject()
         kotlin.runCatching { tokenManager.readAccessToken(token) }
             .onSuccess {
-                val responseMessage = isTokenExpired(token)
+                responseMessage = isTokenExpired(token)
 
                 messageProducer.produceMessage(responseMessage.toString(), KafkaConfig.Wiki_topic)
             }
             .onFailure {
-                val responseMessage = JSONObject()
                 responseMessage.put("status", ResponseStatusEnum.FAILED)
                 responseMessage.put("message", "Token is invalid")
                 messageProducer.produceMessage(responseMessage.toString(), KafkaConfig.Wiki_topic)
             }
-
     }
 
     @KafkaListener(topics = [KafkaConfig.Subscribe_topic], groupId = KafkaConfig.Group_id)
-    fun consumeFromWiki(token: String): Unit {
+    fun consumeFromWiki(token: String) {
+        var responseMessage = JSONObject()
         kotlin.runCatching { tokenManager.readAccessToken(token) }
             .onSuccess {
-                val responseMessage = isTokenExpired(token)
+                responseMessage = isTokenExpired(token)
                 messageProducer.produceMessage(responseMessage.toString(), KafkaConfig.Subscribe_topic)
             }
             .onFailure {
-                val responseMessage = JSONObject()
                 responseMessage.put("status", ResponseStatusEnum.FAILED)
                 responseMessage.put("message", "Token is invalid")
                 messageProducer.produceMessage(responseMessage.toString(), KafkaConfig.Subscribe_topic)

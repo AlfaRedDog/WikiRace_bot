7abdde0d28fe0533fbf2967ae7192c375f8da952
AlfaRedDog <72893891+AlfaRedDog@users.noreply.github.com>
2023-04-06 01:31:01 +0300

    fix auth consumers

--- auth-service/src/main/kotlin/com/itmo/services/kafka/MessageConsumer.kt
+++ auth-service/src/main/kotlin/com/itmo/services/kafka/MessageConsumer.kt
@@ -29,7 +29,10 @@ class MessageConsumer(private val tokenManager: JwtTokenManager, private val mes
         }
     }
 
-    @KafkaListener(topics = [KafkaConfig.Subscribe_topic], groupId = KafkaConfig.Group_id)
+    @KafkaListener(topics = [KafkaConfig.Subscribe_topic], groupId = KafkaConfig.Group_id, properties = [
+        "key.deserializer=org.apache.kafka.common.serialization.StringDeserializer",
+        "value.deserializer=com.itmo.services.kafka.models.AuthRequestMessageDeserializer",
+        "value.deserializer.type=com.itmo.services.kafka.models.AuthRequestMessage"])
     fun consumeFromSubscribe(message: AuthRequestMessage) {
         var responseMessage: UserDetails
         kotlin.runCatching { tokenManager.readAccessToken(message.token) }.onSuccess { user ->

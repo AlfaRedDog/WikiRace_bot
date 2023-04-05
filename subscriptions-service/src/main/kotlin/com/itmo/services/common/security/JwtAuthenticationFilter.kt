0dde16d06d5242aa2d855ef61337a73d938fd5d2
AlfaRedDog <72893891+AlfaRedDog@users.noreply.github.com>
2023-04-05 19:43:33 +0300

    Add auth check to subscription micro-service

--- subscriptions-service/src/main/kotlin/com/itmo/services/common/security/JwtAuthenticationFilter.kt
+++ subscriptions-service/src/main/kotlin/com/itmo/services/common/security/JwtAuthenticationFilter.kt
@@ -1,28 +1,64 @@
 package com.itmo.services.common.security
 
+import com.itmo.services.common.exception.AccessDeniedException
+import com.itmo.services.kafka.MessageProducer
+import com.itmo.services.kafka.config.KafkaConfig
+import com.itmo.services.kafka.models.AuthRequestMessage
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.springframework.kafka.retrytopic.DestinationTopic
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
+import java.time.Duration
+import java.util.*
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 @Component
 class JwtAuthenticationFilter(): OncePerRequestFilter() {
+    val messageProducer : MessageProducer = MessageProducer()
 
     override fun doFilterInternal(request: HttpServletRequest,
                                   response: HttpServletResponse,
                                   filterChain: FilterChain) {
-/*      val token = retrieveToken(request)
+        val token = retrieveToken(request)
         if (token == null) {
-            filterChain.doFilter(request, response)*/
-//            return
-//        }
-//        kotlin.runCatching { tokenManager.readAccessToken(token) }
-//                .onSuccess { user -> SecurityContextHolder.getContext().authentication =
-//                        UsernamePasswordAuthenticationToken(user, token, user.authorities) }
+            filterChain.doFilter(request, response)
+            return
+        }
+        val kafkaProps = Properties()
+        kafkaProps[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = "localhost:9092"
+        kafkaProps[ConsumerConfig.GROUP_ID_CONFIG] = "subscription-group"
+        kafkaProps[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.name
+        kafkaProps[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.name
+        val authId : String = UUID.randomUUID().toString()
+        messageProducer.produceMessage(
+            AuthRequestMessage(token = token, authId = authId),
+            KafkaConfig.Subscribe_topic)
+
+        val consumerTopic = KafkaConfig.Subscribe_topic + " $authId"
+        val consumer = KafkaConsumer<String, UserDetails>(kafkaProps)
+        consumer.subscribe(listOf(consumerTopic))
+
+        kotlin.runCatching {
+                while (true) {
+                    val records = consumer.poll(Duration.ofMillis(10))
+                    for (record: ConsumerRecord<String, UserDetails> in records) {
+                        if(record.value().authorities != mutableListOf(SimpleGrantedAuthority("ACCESS")))
+                           throw AccessDeniedException()
+                    }
+                }
+            }
+            .onSuccess { user -> SecurityContextHolder.getContext().authentication =
+                UsernamePasswordAuthenticationToken(user, token, mutableListOf(SimpleGrantedAuthority("ACCESS"))) }
         filterChain.doFilter(request, response)
-        return
     }
 }
--- subscriptions-service/src/main/kotlin/com/itmo/services/kafka/MessageProducer.kt
+++ subscriptions-service/src/main/kotlin/com/itmo/services/kafka/MessageProducer.kt
@@ -1,5 +1,6 @@
 package com.itmo.services.kafka
 
+import com.itmo.services.kafka.models.AuthRequestMessage
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.clients.producer.RecordMetadata
@@ -9,14 +10,14 @@ import java.util.concurrent.Future
 
 @Component
 class MessageProducer {
-    fun produceMessage(message: String, topic: String): ResponseEntity<String> {
-        val producerRecord: ProducerRecord<String, String> = ProducerRecord(topic, message)
+    fun produceMessage(message: AuthRequestMessage, topic: String): ResponseEntity<String> {
+        val producerRecord: ProducerRecord<String, AuthRequestMessage> = ProducerRecord(topic, message)
 
         val map = mutableMapOf<String, String>()
         map["key.serializer"] = "org.apache.kafka.common.serialization.StringSerializer"
         map["value.serializer"] = "org.apache.kafka.common.serialization.StringSerializer"
 
-        val producer = KafkaProducer<String, String>(map as Map<String, Any>?)
+        val producer = KafkaProducer<String, AuthRequestMessage>(map as Map<String, Any>?)
         val future: Future<RecordMetadata> = producer.send(producerRecord)!!
 
         return ResponseEntity.ok(" message sent to " + future.get().topic());

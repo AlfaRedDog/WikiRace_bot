0fa233ef7769e719ccb0b7e3be16841564c8153f
Igor <fdiii@mail.ru>
2023-04-05 14:13:01 +0300

    Small refactor

--- auth-service/src/main/kotlin/com/itmo/services/common/DefaultResponse.kt
+++ auth-service/src/main/kotlin/com/itmo/services/common/DefaultResponse.kt
@@ -3,6 +3,6 @@ package com.itmo.services.common
 import com.itmo.services.common.enums.ResponseStatus
 
 data class DefaultResponse(
-    var status : ResponseStatus,
-    var errors : Array<String>
-    )
+    var status: ResponseStatus,
+    var errors: Array<String>
+)

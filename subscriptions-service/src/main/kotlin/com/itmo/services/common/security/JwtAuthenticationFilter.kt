8bfbbf55cf4286a07817274c6e872ba6dfd6cdd3
Igor <fdiii@mail.ru>
2023-04-05 14:09:54 +0300

    Delete auth

--- subscriptions-service/src/main/kotlin/com/itmo/services/common/security/JwtAuthenticationFilter.kt
+++ subscriptions-service/src/main/kotlin/com/itmo/services/common/security/JwtAuthenticationFilter.kt
@@ -1,6 +1,5 @@
 package com.itmo.services.common.security
 
-import com.itmo.services.auth.impl.service.JwtTokenManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
@@ -10,19 +9,19 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 @Component
-class JwtAuthenticationFilter(private val tokenManager: JwtTokenManager): OncePerRequestFilter() {
+class JwtAuthenticationFilter(): OncePerRequestFilter() {
 
     override fun doFilterInternal(request: HttpServletRequest,
                                   response: HttpServletResponse,
                                   filterChain: FilterChain) {
-        val token = retrieveToken(request)
-        if (token == null) {
-            filterChain.doFilter(request, response)
-            return
-        }
-        kotlin.runCatching { tokenManager.readAccessToken(token) }
-                .onSuccess { user -> SecurityContextHolder.getContext().authentication =
-                        UsernamePasswordAuthenticationToken(user, token, user.authorities) }
-        filterChain.doFilter(request, response)
+//        val token = retrieveToken(request)
+//        if (token == null) {
+//            filterChain.doFilter(request, response)
+//            return
+//        }
+//        kotlin.runCatching { tokenManager.readAccessToken(token) }
+//                .onSuccess { user -> SecurityContextHolder.getContext().authentication =
+//                        UsernamePasswordAuthenticationToken(user, token, user.authorities) }
+//        filterChain.doFilter(request, response)
     }
 }
--- subscriptions-service/src/main/kotlin/com/itmo/services/kafka/MessageConsumer.kt
+++ subscriptions-service/src/main/kotlin/com/itmo/services/kafka/MessageConsumer.kt
@@ -1,6 +1,5 @@
 package com.itmo.services.kafka
 
-import com.itmo.services.auth.impl.service.JwtTokenManager
 import com.itmo.services.kafka.config.KafkaConfig
 import com.itmo.services.kafka.models.ResponseStatusEnum
 import org.springframework.boot.configurationprocessor.json.JSONObject
@@ -9,52 +8,6 @@ import org.springframework.stereotype.Service
 
 
 @Service
-class MessageConsumer(private val tokenManager: JwtTokenManager, private val messageProducer: MessageProducer) {
-    @KafkaListener(topics = [KafkaConfig.Wiki_topic], groupId = KafkaConfig.Group_id)
-    fun consumeFromSubscribe(token: String) {
-        var responseMessage = JSONObject()
-        kotlin.runCatching { tokenManager.readAccessToken(token) }
-            .onSuccess {
-                responseMessage = isTokenExpired(token)
+class MessageConsumer(private val messageProducer: MessageProducer) {
 
-                messageProducer.produceMessage(responseMessage.toString(), KafkaConfig.Wiki_topic)
-            }
-            .onFailure {
-                responseMessage.put("status", ResponseStatusEnum.FAILED)
-                responseMessage.put("message", "Token is invalid")
-                messageProducer.produceMessage(responseMessage.toString(), KafkaConfig.Wiki_topic)
-            }
-    }
-
-    @KafkaListener(topics = [KafkaConfig.Subscribe_topic], groupId = KafkaConfig.Group_id)
-    fun consumeFromWiki(token: String) {
-        var responseMessage = JSONObject()
-        kotlin.runCatching { tokenManager.readAccessToken(token) }
-            .onSuccess {
-                responseMessage = isTokenExpired(token)
-                messageProducer.produceMessage(responseMessage.toString(), KafkaConfig.Subscribe_topic)
-            }
-            .onFailure {
-                responseMessage.put("status", ResponseStatusEnum.FAILED)
-                responseMessage.put("message", "Token is invalid")
-                messageProducer.produceMessage(responseMessage.toString(), KafkaConfig.Subscribe_topic)
-            }
-    }
-
-    private fun isTokenExpired(token: String): JSONObject {
-        val isExpired = tokenManager.isTokenExpired(token)
-
-        val status = if (!isExpired) ResponseStatusEnum.SUCCESS else ResponseStatusEnum.FAILED
-
-        val message = when (status) {
-            ResponseStatusEnum.SUCCESS -> ""
-            ResponseStatusEnum.FAILED -> "Token is expired"
-        }
-
-        val responseMessage = JSONObject()
-        responseMessage.put("status", status)
-        responseMessage.put("message", message)
-
-        return responseMessage
-    }
 }

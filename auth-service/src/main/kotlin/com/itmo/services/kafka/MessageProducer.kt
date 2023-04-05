b9ab845271980a54a390bcd0c989c3868862ee53
Igor <fdiii@mail.ru>
2023-04-05 22:52:36 +0300

    Fix config

--- auth-service/src/main/kotlin/com/itmo/services/kafka/MessageProducer.kt
+++ auth-service/src/main/kotlin/com/itmo/services/kafka/MessageProducer.kt
@@ -1,9 +1,7 @@
 package com.itmo.services.kafka
 
-import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.KafkaProducer
-import org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG
-import org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG
+import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.springframework.kafka.support.serializer.JsonDeserializer
 import org.springframework.security.core.userdetails.UserDetails
@@ -15,9 +13,9 @@ class MessageProducer {
         val producerRecord: ProducerRecord<String, UserDetails> = ProducerRecord(topic, message)
 
         val map = mutableMapOf<String, String>()
-        map[KEY_SERIALIZER_CLASS_CONFIG] = "org.apache.kafka.common.serialization.StringSerializer"
-        map[VALUE_SERIALIZER_CLASS_CONFIG] = JsonDeserializer::class.java.name
-        map[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = UserDetails::class.java.name
+        map[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = "org.apache.kafka.common.serialization.StringSerializer"
+        map[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JsonDeserializer::class.java.name
+        map[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG + ".type"] = UserDetails::class.java.name
 
         val producer = KafkaProducer<String, UserDetails>(map as Map<String, Any>?)
         producer.send(producerRecord)
--- subscriptions-service/src/main/kotlin/com/itmo/services/kafka/MessageProducer.kt
+++ subscriptions-service/src/main/kotlin/com/itmo/services/kafka/MessageProducer.kt
@@ -2,9 +2,11 @@ package com.itmo.services.kafka
 
 import com.itmo.services.kafka.models.AuthRequestMessage
 import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.clients.producer.RecordMetadata
 import org.springframework.http.ResponseEntity
+import org.springframework.kafka.support.serializer.JsonDeserializer
 import org.springframework.stereotype.Component
 import java.util.concurrent.Future
 
@@ -14,12 +16,13 @@ class MessageProducer {
         val producerRecord: ProducerRecord<String, AuthRequestMessage> = ProducerRecord(topic, message)
 
         val map = mutableMapOf<String, String>()
-        map["key.serializer"] = "org.apache.kafka.common.serialization.StringSerializer"
-        map["value.serializer"] = "org.apache.kafka.common.serialization.StringSerializer"
+        map[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = "org.apache.kafka.common.serialization.StringSerializer"
+        map[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JsonDeserializer::class.java.name
+        map[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG + ".type"] = AuthRequestMessage::class.java.name
 
         val producer = KafkaProducer<String, AuthRequestMessage>(map as Map<String, Any>?)
         val future: Future<RecordMetadata> = producer.send(producerRecord)!!
 
-        return ResponseEntity.ok(" message sent to " + future.get().topic());
+        return ResponseEntity.ok(" message sent to " + future.get().topic())
     }
 }

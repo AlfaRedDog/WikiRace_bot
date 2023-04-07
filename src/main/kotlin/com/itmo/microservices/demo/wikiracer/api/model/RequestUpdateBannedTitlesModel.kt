322f68b2f13e9e7ca9c62c9a2ef7bdd935c5bb91
Igor <fdiii@mail.ru>
2023-04-07 13:54:00 +0300

    Delete unused methods

--- src/main/kotlin/com/itmo/microservices/demo/wikiracer/api/model/RequestUpdateBannedTitlesModel.kt
+++
@@ -1,5 +0,0 @@
-package com.itmo.microservices.demo.wikiracer.api.model
-
-import java.util.*
-
-data class RequestUpdateBannedTitlesModel(val userId: String, val titles: List<String>, val createTime: Date)
--- src/main/kotlin/com/itmo/microservices/demo/wikiracer/impl/model/WikiRacerAggregate.kt
+++
@@ -1,8 +0,0 @@
-package com.itmo.microservices.demo.wikiracer.impl.model
-
-import ru.quipy.core.annotations.AggregateType
-import ru.quipy.domain.Aggregate
-
-@AggregateType("wiki_racer")
-class WikiRacerAggregate : Aggregate {
-}

e1b0a194fe2cfee37ecd1fc3391186376c08f749
AlfaRedDog <72893891+AlfaRedDog@users.noreply.github.com>
2023-04-08 16:08:53 +0300

    add getSubscriptionLevel endpoint

--- subscriptions-service/src/main/kotlin/com/itmo/services/subscriptions/api/controller/SubscriptionsController.kt
+++ subscriptions-service/src/main/kotlin/com/itmo/services/subscriptions/api/controller/SubscriptionsController.kt
@@ -1,5 +1,6 @@
 package com.itmo.services.subscriptions.api.controller
 
+import com.itmo.services.subscriptions.api.models.SubscriptionLevel
 import com.itmo.services.subscriptions.api.models.UpdateSubscriptionRequest
 import com.itmo.services.subscriptions.impl.service.SubscriptionService
 import io.swagger.v3.oas.annotations.Operation
@@ -7,16 +8,13 @@ import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import kotlinx.coroutines.runBlocking
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/subscriptions")
 class SubscriptionsController(private val subscriptionService : SubscriptionService) {
 
-    @PostMapping
+    @PostMapping("/update")
     @Operation(
         summary = "Update subscription level",
         responses = [
@@ -28,4 +26,17 @@ class SubscriptionsController(private val subscriptionService : SubscriptionServ
     fun updateSubscriptions (
         @RequestBody request : UpdateSubscriptionRequest
     ) = runBlocking { subscriptionService.updateSubscriptionLevel(request) }
+
+    @PostMapping("/get")
+    @Operation(
+        summary = "Get subscription level",
+        responses = [
+            ApiResponse(description = "OK", responseCode = "200"),
+            ApiResponse(description = "Bad request", responseCode = "400", content = [Content()])
+        ],
+        security = [SecurityRequirement(name = "bearerAuth")]
+    )
+    fun getSubscriptionLevel(@RequestBody userId: String) : SubscriptionLevel {
+      return subscriptionService.getSubscriptionInfoByUsername(userId)
+    }
 }
